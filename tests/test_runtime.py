6f772968d4e8323206506c75dfa12878a53eb7c7
Sean Evans <sean.w.evans@gmail.com>
2025-06-18 15:04:04 -0400

    Add unsigned saturation tests

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -43,5 +43,47 @@ def test_sat_mul_saturates_min():
     assert saturated
 
 
+def test_sat_add_unsigned_normal():
+    value, saturated = rt.sat_add(10, 20, 8, signed=False)
+    assert value == 30
+    assert not saturated
+
+
+def test_sat_add_unsigned_saturates_max():
+    value, saturated = rt.sat_add(250, 20, 8, signed=False)
+    assert value == 255
+    assert saturated
+
+
+def test_sat_sub_unsigned_normal():
+    value, saturated = rt.sat_sub(20, 10, 8, signed=False)
+    assert value == 10
+    assert not saturated
+
+
+def test_sat_sub_unsigned_saturates_min():
+    value, saturated = rt.sat_sub(10, 20, 8, signed=False)
+    assert value == 0
+    assert saturated
+
+
+def test_sat_mul_unsigned_normal():
+    value, saturated = rt.sat_mul(5, 4, 8, signed=False)
+    assert value == 20
+    assert not saturated
+
+
+def test_sat_mul_unsigned_saturates_max():
+    value, saturated = rt.sat_mul(20, 20, 8, signed=False)
+    assert value == 255
+    assert saturated
+
+
+def test_sat_mul_unsigned_saturates_min():
+    value, saturated = rt.sat_mul(-20, 20, 8, signed=False)
+    assert value == 0
+    assert saturated
+
+
 def test_no_saturating_overflow_export():
     assert not hasattr(rt, "SaturatingOverflow")
