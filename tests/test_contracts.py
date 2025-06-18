8ead4c50cd6a2be522cb4d94fed10641243f0033
Sean Evans <sean.w.evans@gmail.com>
2025-06-18 15:06:13 -0400

    Remove local sys.path hack

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -2,7 +2,6 @@ import os
 import sys
 import pytest
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from safelang.parser import parse_functions, verify_contracts
 
 
